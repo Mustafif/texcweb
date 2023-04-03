74d3de213a169bff17b4474d67323eb41ac99ea3
Mustafif <mustafif0929@gmail.com>
2023-04-03 10:15:34 -0400

    fixed sh function

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,64 @@
-mod repos;
 mod base;
+mod repos;
 
-use std::future::Future;
-use rocket::fs::{FileServer, NamedFile};
-use rocket::{launch, get, routes, post};
-use rocket::form::Form;
-use texc_v3_web::{Mode, WebConfig};
 use crate::repos::build_index;
+use rocket::form::Form;
+use rocket::fs::{FileServer, NamedFile};
+use rocket::{get, launch, post, routes};
+use tokio::fs::File;
+use tokio::io::AsyncWriteExt;
+use std::future::Future;
+use texc_v3_web::{Mode, WebConfig};
 
 // Current release number
-pub(crate) fn current() -> u64{
+pub(crate) fn current() -> u64 {
     2
 }
 
 #[get("/repo/latest")]
-async fn get_latest() -> String{
+async fn get_latest() -> String {
     format!("{}", current())
 }
 
 #[get("/sh")]
-async fn sh() -> Option<NamedFile>{
+async fn sh() -> Option<NamedFile> {
+    let s = r#"
+    #!/bin/sh 
+    sudo -s 
+    echo "deb [arch=amd64, trusted=yes] https://mkproj.github.io/texcreate-deb texcreate-deb main" >> /etc/apt/sources.list
+    apt-get update && apt-get upgrade 
+    apt-get install texcreate   
+    "#;
+    let mut f = File::create("texcreate-install.sh").await.unwrap();
+    f.write_all(s.as_bytes()).await.unwrap();
     NamedFile::open("texcreate-install.sh").await.ok()
 }
 
 #[get("/create")]
-async fn build_texc() -> Option<NamedFile>{
-    match texc_v3_web::build_index(Some("texcreate.html".to_string()), Some(Mode::Light)).await.ok(){
-       Some(()) => (),
-        None => return None
+async fn build_texc() -> Option<NamedFile> {
+    match texc_v3_web::build_index(Some("texcreate.html".to_string()), Some(Mode::Light))
+        .await
+        .ok()
+    {
+        Some(()) => (),
+        None => return None,
     };
     NamedFile::open("texcreate.html").await.ok()
 }
 
 #[post("/", data = "<input>")]
-async fn create_proj(input: Form<WebConfig>) -> Option<NamedFile>{
+async fn create_proj(input: Form<WebConfig>) -> Option<NamedFile> {
     let input = input.into_inner();
     let file = input.zip().await.ok();
-    match file{
+    match file {
         Some(name) => NamedFile::open(&name).await.ok(),
-        None => None
+        None => None,
     }
 }
 
 #[get("/")]
-async fn index() -> Option<NamedFile>{
-    match build_index().await.ok(){
+async fn index() -> Option<NamedFile> {
+    match build_index().await.ok() {
         Some(()) => (),
         None => return None,
     }
@@ -52,7 +66,7 @@ async fn index() -> Option<NamedFile>{
 }
 
 #[launch]
-fn rocket() -> _{
+fn rocket() -> _ {
     rocket::build()
         .mount("/", routes![index, sh, get_latest, build_texc, create_proj])
         .mount("/assets", FileServer::from("static"))
--- src/repos.rs
+++ src/repos.rs
@@ -1,30 +1,29 @@
-use std::collections::HashMap;
-use tokio::io::{AsyncWriteExt, Result};
-use texcreate_repo::Repo;
-use tokio::fs::File;
 use crate::base::BASE;
 use crate::current;
+use std::collections::HashMap;
+use texcreate_repo::Repo;
+use tokio::fs::File;
+use tokio::io::{AsyncWriteExt, Result};
 
-
-fn link(version: u64) -> String{
+fn link(version: u64) -> String {
     format!("https://github.com/MKProj/mkproj_texcgen/releases/tag/v{version}")
 }
 
-fn repo_link(version: u64) -> String{
+fn repo_link(version: u64) -> String {
     format!("https://github.com/MKProj/mkproj_texcgen/releases/download/v{version}/repo.toml")
 }
 
 #[derive(Debug, Clone)]
-pub struct CurrentRelease{
+pub struct CurrentRelease {
     version: u64,
-    info: HashMap<String, String>
+    info: HashMap<String, String>,
 }
 
-impl CurrentRelease{
-    pub fn to_html(&self) -> String{
+impl CurrentRelease {
+    pub fn to_html(&self) -> String {
         let mut parts = Vec::new();
 
-        for (name, desc) in self.info.iter(){
+        for (name, desc) in self.info.iter() {
             parts.push("<tr>".to_string());
             parts.push(format!("<td>{name}</td>"));
             parts.push(format!("<td>{desc}</td>"));
@@ -36,26 +35,29 @@ impl CurrentRelease{
 }
 
 #[derive(Debug, Clone)]
-pub struct Release{
+pub struct Release {
     version: u64,
     num: u64,
     info: HashMap<String, String>,
     link: String,
 }
 
-impl Release{
-    pub fn new(repo: &Repo) -> Self{
-        Self{
+impl Release {
+    pub fn new(repo: &Repo) -> Self {
+        Self {
             version: repo.version(),
             num: repo.num(),
             info: repo.info(),
-            link: link(repo.version())
+            link: link(repo.version()),
         }
     }
-    pub fn as_current(&self) -> CurrentRelease{
-        CurrentRelease{version: self.version, info: self.info.clone()}
+    pub fn as_current(&self) -> CurrentRelease {
+        CurrentRelease {
+            version: self.version,
+            info: self.info.clone(),
+        }
     }
-    pub fn to_html(&self) -> String{
+    pub fn to_html(&self) -> String {
         let mut parts = Vec::new();
         parts.push("<tr>".to_string());
         parts.push(format!("<td>{}</td>", self.version));
@@ -67,37 +69,37 @@ impl Release{
 }
 
 #[derive(Debug, Clone)]
-pub struct Releases{
+pub struct Releases {
     rels: Vec<Release>,
 }
 
-impl Releases{
-    pub async fn new() -> Self{
+impl Releases {
+    pub async fn new() -> Self {
         let mut rels = Vec::new();
 
         let curr = current();
-        for i in 0..=curr{
+        for i in 0..=curr {
             let repo = Repo::get_repo(&repo_link(i)).await;
             let release = Release::new(&repo);
             rels.push(release);
         }
-        Self{rels}
+        Self { rels }
     }
-    pub fn get_current(&self) -> CurrentRelease{
+    pub fn get_current(&self) -> CurrentRelease {
         let mut releases = self.clone();
         let release = releases.rels.pop().expect("Couldn't get current release");
         release.as_current()
     }
-    pub fn to_html(&self) -> String{
+    pub fn to_html(&self) -> String {
         let mut parts = Vec::new();
-        for r in &self.rels{
+        for r in &self.rels {
             parts.push(r.to_html());
         }
         parts.join("\n")
     }
 }
 
-pub async fn build_index() -> Result<()>{
+pub async fn build_index() -> Result<()> {
     let mut base = BASE.to_string();
     let releases = Releases::new().await;
     let curr = releases.get_current();
